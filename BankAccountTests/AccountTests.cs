36d3ff3379cd68f4fff36b5c2cd867cbb6a5ac54
Zalman Izak <thezalmanian@gmail.com>
2023-02-01 20:38:42 -0800

    Added a unit test to test the Deposit method (not implemented yet)

---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void Deposit_APositiveAmount_AddToBalance()
+        {
+            Assert.Fail();
+        }
+    }
+}
