b3fe95e2dc807f0e12497307724f0d3114f0f8d0
Zalman Izak <thezalmanian@gmail.com>
2023-02-01 20:58:11 -0800

    Added unit test to test the return value of the Deposit method

--- BankAccountTests/AccountTests.cs
+++ BankAccountTests/AccountTests.cs
@@ -8,10 +8,10 @@ using System.Threading.Tasks;
 
 namespace BankAccount.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class AccountTests
     {
-        [TestMethod()]
+        [TestMethod]
         public void Deposit_APositiveAmount_AddToBalance()
         {
             // Create a test account
@@ -22,6 +22,28 @@ namespace BankAccount.Tests
 
             // Check if the deposit was successful
             Assert.AreEqual(100, testAccount.Balance);
+        }
+
+        [TestMethod]
+        public void Deposit_APositiveAmount_ReturnsUpdatedBalance()
+        {
+            // AAA - Arrange, Act, Assert
+
+            // Arrange:
+            // Create a test account
+            Account testAccount = new("Reality Undefined");
+
+            // Setup deposit amount, and expected return amount
+            double depositAmount = 100;
+            double expectedReturn = 100;
+
+            // Act
+            // Deposit $100 into the account
+            double returnValue = testAccount.Deposit(100);
+
+            // Assert 
+            // Make sure the return value was $100
+            Assert.AreEqual(expectedReturn, returnValue);
         }
     }
 }
