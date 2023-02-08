2a9180729927d8cba9a88365821b29b036e9df40
Zalman Izak <thezalmanian@gmail.com>
2023-02-07 21:13:16 -0800

    Implemented IsValueWithinRangeTest_OutOfRangeValue_ReturnsFalse unit test

--- BankAccountTests/ValidatorTests.cs
+++ BankAccountTests/ValidatorTests.cs
@@ -26,5 +26,23 @@ namespace BankAccount.Tests
             // See if the result was True
             Assert.IsTrue(result);
         }
+
+        [TestMethod()]
+        [DataRow(-1, 0, 100)]
+        [DataRow(-.01, 0, 100)]
+        [DataRow(100.01, 0, 100)]
+        [DataRow(101, 0, 100)]
+        [DataRow(7, 12, 32)]
+        [TestCategory("Is Within Range")]
+        public void IsValueWithinRangeTest_OutOfRangeValue_ReturnsFalse(double value,
+                                                                        double minValue,
+                                                                        double maxValue)
+        {
+            // Check if the given value is not within the given range
+            bool result = Validator.IsValueWithinRange(value, minValue, maxValue);
+
+            // See if the result was False
+            Assert.IsFalse(result);
+        }
     }
 }
